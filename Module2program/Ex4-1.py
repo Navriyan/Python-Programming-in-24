a0bff3a528a8f0d87416589fb44c1d6e1533ace1
Navdeep Kaur <nkaur40@ivytech.edu>
2024-06-20 15:24:56 -0400

    added comment

--- Module2program/Ex4-1.py
+++ Module2program/Ex4-1.py
@@ -2,6 +2,7 @@ secret = int(input("enter the value from 1 to 10 :"))
 guess =int(input("enter the value from 1 to 10 :"))
 if guess< secret:
     print("too low")
+    #comment
 elif guess> secret:
         print("too high")
 else :
